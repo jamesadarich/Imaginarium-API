be2cc147e907f399ad79f8b03e99362b94902ce3
jamesrichford <jamesrichford@googlemail.com>
2015-01-20 23:26:01 +0000

    Reattach disconnected files

---
+++ DataTransferObjects/AndroidReport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    public class AndroidReport
+    {
+        [DataMember]
+        public string STACK_TRACE;
+
+        [DataMember]
+        public DateTime USER_APP_START_DATE;
+
+        [DataMember]
+        public DateTime USER_CRASH_DATE;
+    }
+}
---
+++ Imaginarium API/error-reporting/AndroidReportController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Imaginarium_API.error_reporting
+{
+    public class AndroidReportController : ApiController
+    {
+        private static ICollection<DataTransferObjects.AndroidReport> reports;
+
+        [HttpGet]
+        [Route("error-reporting/reports/android")]
+        public IEnumerable<DataTransferObjects.AndroidReport> Get()
+        {
+            if (reports == null)
+            {
+                reports = new List<DataTransferObjects.AndroidReport>();
+            }
+
+            return reports.AsEnumerable();
+        }
+
+        [HttpPut]
+        [Route("error-reporting/report/android")]
+        public DataTransferObjects.AndroidReport Put([FromBody] DataTransferObjects.AndroidReport report)
+        {
+            if (reports == null)
+            {
+                reports = new List<DataTransferObjects.AndroidReport>();
+            }
+
+            reports.Add(report);
+
+            return report;
+
+        }
+    }
+}
