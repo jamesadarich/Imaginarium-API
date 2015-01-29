46242a393c38a8da0a938aa60e1ebcec3b7a0a46
jamesrichford <jamesrichford@googlemail.com>
2015-01-29 19:08:27 +0000

    Pattern Builder shell

---
+++ Imaginarium API/pattern-builder/PatternController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Imaginarium_API.pattern_builder
+{
+    public class PatternController : ApiController
+    {
+        [HttpGet]
+        [Route("pattern-builder/patterns")]
+        public IEnumerable<DataTransferObjects.PatternBuilder.Pattern> GetAll()
+        {
+            return new List<DataTransferObjects.PatternBuilder.Pattern>();
+        }
+
+        [HttpGet]
+        [Route("pattern-builder/pattern/{patternId}")]
+        public DataTransferObjects.PatternBuilder.Pattern Get(Guid patternId)
+        {
+            return new DataTransferObjects.PatternBuilder.Pattern();
+        }
+
+        [HttpPost]
+        [Route("pattern-builder/pattern")]
+        public DataTransferObjects.PatternBuilder.Pattern Post([FromBody] DataTransferObjects.PatternBuilder.Pattern pattern)
+        {
+            return pattern;
+        }
+
+        [HttpPut]
+        [Route("pattern-builder/pattern")]
+        public DataTransferObjects.PatternBuilder.Pattern Put([FromBody] DataTransferObjects.PatternBuilder.Pattern pattern)
+        {
+            return pattern;
+        }
+
+        [HttpDelete]
+        [Route("pattern-builder/pattern/{patternId}")]
+        public void Delete(Guid patternId)
+        {
+
+        }
+    }
+}
