3bf79b2a3e2cd6a3481162e55424470155cc3fe2
jamesrichford <jamesrichford@googlemail.com>
2015-01-31 13:04:22 +0000

    Legends of Lunchtime - Mock Login

---
+++ Imaginarium API/legends-of-lunchtime/SessionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Imaginarium_API.legends_of_lunchtime
+{
+    public class SessionController : ApiController
+    {
+        [HttpGet]
+        [Route("legends-of-lunchtime/login")]
+        public DataTransferObjects.LegendsOfLunchtime.Session Get(string username, string password)
+        {
+            var session = new DataTransferObjects.LegendsOfLunchtime.Session();
+
+            session.Id = Guid.NewGuid();
+            session.ExpiresOn = DateTime.UtcNow.AddMinutes(10);
+
+            return session;
+        }
+    }
+}
