488ff6b44f95ef292acc7af336d2303670f66e2e
jamesrichford <jamesrichford@googlemail.com>
2015-10-23 21:30:18 +0100

    Added error logging on the android manager

--- Managers/ErrorReaper/AndroidReportManager.cs
+++ Managers/ErrorReaper/AndroidReportManager.cs
@@ -10,15 +10,43 @@ namespace ErrorReaper.Managers
 {
     public class AndroidReportManager
     {
-        public DataTransferObjects.AndroidReport Update(DataTransferObjects.AndroidReport report)
+        public DataTransferObjects.AndroidReport Update(DataTransferObjects.AndroidReport report)        
         {
-            var model = report.ToModel();
+            try
+            {
+                var model = report.ToModel();
 
-            var repository = new DataAccess.Repository();
-            repository.AndroidReports.Add(model);
-            repository.SaveChanges();
+                var repository = new DataAccess.Repository();
+                var existingModel = repository.AndroidReports.SingleOrDefault(m => m.Id == model.Id);
+                if (existingModel != null)
+                {
+                    existingModel.LogCat = model.LogCat;
+                    existingModel.CrashDate = model.CrashDate;
+                    existingModel.PackageName = model.PackageName;
+                    existingModel.StackTrace = model.StackTrace;
+                    repository.Entry(existingModel).State = System.Data.Entity.EntityState.Modified;
+                }
+                else
+                {
+                    repository.AndroidReports.Add(model);
+                }
+                repository.SaveChanges();
 
-            return report;
+                return report;
+            }
+            catch (Exception e)
+            {
+                var dotNetReport = new DataTransferObjects.DotNetReport();
+                dotNetReport.Application = new DataTransferObjects.Application();
+                dotNetReport.Application.Name = "Android Report Manager";
+                dotNetReport.Exception = new DataTransferObjects.DotNetException();
+                dotNetReport.Exception.Message = e.Message;
+                dotNetReport.Exception.StackTrace = e.StackTrace;
+                dotNetReport.Timestamp = DateTime.UtcNow;
+                var dotNetManager = new DotNetReportManager();
+                dotNetManager.Create(dotNetReport);
+                throw e;
+            }
         }
 
         public IEnumerable<DataTransferObjects.AndroidReport> GetAll(string sort, int skip, int take)
